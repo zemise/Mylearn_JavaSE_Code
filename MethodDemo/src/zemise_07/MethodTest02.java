2621a9852d90220b39ff9551508e83b4a1fb0878
Wang Zhao <373933306@qq.com>
2022-08-31 16:50:28 +0800

    打卡 P89

---
+++ MethodDemo/src/zemise_07/MethodTest02.java
@@ -0,0 +1,42 @@
+package zemise_07;
+/*
+    数组最大值
+    需求：
+        设计一个方法用于获取数组元素中的最大值，调用方法并输出结果
+    思路
+        1、定义一个数组，用静态初始化完成数组元素初始化
+        2、定义一个方法，用来获取数组中的最大值
+        3、调用获取最大值方法，用变量接收返回结果
+        4、把结果输出在控制台
+ */
+
+public class MethodTest02 {
+    public static void main(String[] args) {
+        //定义一个数组，用静态初始化完成数组元素初始化
+        int[] arr = {11, 45, 98, 73, 60};
+
+        //调用获取最大值方法，用变量接收返回结果
+        int number = getMax(arr);
+
+        //把结果输出在控制台
+        System.out.println("该数组内元素最大值为：" + number);
+
+    }
+
+    //定义一个方法，用来获取数组中的最大值
+    /*
+        两个明确：
+            返回值类型
+            参数
+     */
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+
+        for (int x = 1; x < arr.length; x++) {
+            if (arr[x] > max) {
+                max = arr[x];
+            }
+        }
+    return max;
+    }
+}
