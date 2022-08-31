af7b3164cce1962273e0a6be1a9ce168087eac12
Wang Zhao <373933306@qq.com>
2022-08-31 13:47:16 +0800

    打卡 P84_

--- MethodDemo/src/zemise_07/MethodTest01.java
+++ MethodDemo/src/zemise_07/MethodTest01.java
@@ -26,5 +26,32 @@ public class MethodTest01 {
         System.out.print("Hello");
         System.out.print("World");
         System.out.println();
+
+        //定义一个数组，用静态初始化完成数组元素初始化
+        int[] arr = {11,22,33,44,55};
+        //调用方法
+        printArray(arr);
+
+    }
+
+    //定义一个方法，用数组遍历通用格式对数组进行遍历
+    /*
+        两个明确：
+            返回值类型：void
+            参数：int[] arr
+     */
+
+/*    public static void printArray(int[] arr){
+        for(int x =0 ; x<arr.length; x++){
+            System.out.println(arr[x]);
+        }
+    }*/
+
+    public static void printArray(int[] arr){
+        System.out.print("[");
+        for(int x = 0;x<arr.length; x++){
+            System.out.print(arr[x]);
+        }
+        System.out.println("]");
     }
 }
