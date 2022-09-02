94555c471b33401cae9d7e85ea070dd9dddf15a2
Wang Zhao <373933306@qq.com>
2022-09-02 11:52:32 +0800

    update

---
+++ myClass/src/zemise_02/Student.java
@@ -0,0 +1,14 @@
+package zemise_02;
+/*
+    学生类
+ */
+public class Student {
+    //成员变量
+    String name;
+    int age;
+
+    public void study(){
+        System.out.println("好好学习，天天向上");
+    }
+
+}
