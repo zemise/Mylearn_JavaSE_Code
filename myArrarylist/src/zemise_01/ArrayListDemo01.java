35638c204125ad3208c56f78e7201721306c1d04
Wang Zhao <373933306@qq.com>
2022-09-05 23:32:13 +0800

    update

---
+++ myArrarylist/src/zemise_01/ArrayListDemo01.java
@@ -0,0 +1,35 @@
+package zemise_01;
+/*
+    ArrayList构造方法:
+        public Arraylist():创建一个空的集合对象
+    Arraylist添加方法:
+        public boolean add(E e):将指定的元素追加到此集合的末尾
+        public void add(int index, E element):在此集合中的指定位置插入指定的元素
+ */
+
+import java.util.ArrayList;
+
+public class ArrayListDemo01 {
+    public static void main(String[] args) {
+        //public Arraylist():创建一个空的集合对象
+//        ArrayList<String> array = new ArrayList<>();
+        ArrayList<String> array = new ArrayList<String>();
+
+        //public boolean add(E e):将指定的元素追加到此集合的末尾
+//        System.out.println(array.add("hello"));
+        array.add("hello");
+        array.add("world");
+        array.add("java");
+
+        //public void add(int index, E element):在此集合中的指定位置插入指定的元素
+        array.add(1, "javase");
+//        array.add(3, "javaee");
+
+        //IndexOutOfBoundsException
+//        array.add(4,"test");
+
+
+        //输出集合
+        System.out.println("array: " + array);
+    }
+}
