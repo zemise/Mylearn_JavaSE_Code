5afebf80db9a81e7b6b82b41e1b5ab679450fbd9
Wang Zhao <373933306@qq.com>
2022-09-14 10:18:11 +0800

    打卡 P171

---
+++ MyDuoTai/src/zemise_03/AnimalOperator.java
@@ -0,0 +1,22 @@
+package zemise_03;
+
+/*
+    测试类
+ */
+public class AnimalOperator {
+/*
+    public void useAniaml(Cat c){
+        c.eat();
+    }
+
+    public void useAniaml(Dog d){
+        d.eat();
+    }*/
+
+    public void useAnimal(Animal a){
+        //Animal a = new Cat();
+        //Animal a = new Dog();
+        a.eat();
+//        a.lookDoor();
+    }
+}
