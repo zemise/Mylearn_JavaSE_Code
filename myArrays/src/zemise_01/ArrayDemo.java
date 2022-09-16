7c700c9798dd665da18c580169f531d166a76358
Wang Zhao <373933306@qq.com>
2022-09-16 11:04:05 +0800

    打卡 P200

---
+++ myArrays/src/zemise_01/ArrayDemo.java
@@ -0,0 +1,81 @@
+package zemise_01;
+
+/*
+    冒泡排序：
+    一种排序的方式。对要进行排序的数据中相邻的数据进行两两比较，将较大的数据放在后面，
+     依次对所有的数据进行操作，直至所有数据按要求完成排序
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //定义一个数组
+        int[] arr = {24, 69, 80, 57, 13};
+
+/*        //第一次比较
+        for (int i = 0; i < arr.length - 1 - 0; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.println("第一次比较后：" + arrayToString(arr));
+
+        //第二次比较
+        for (int i = 0; i < arr.length - 1 - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.println("第二次比较后：" + arrayToString(arr));
+
+
+        //第三次比较
+        for (int i = 0; i < arr.length - 1 - 2; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.println("第三次比较后：" + arrayToString(arr));
+
+        //第四次比较
+        for (int i = 0; i < arr.length - 1 - 3; i++) {
+            if (arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        System.out.println("第四次比较后：" + arrayToString(arr));*/
+
+        for (int x = 0; x < arr.length - 1; x++) {
+            for (int i = 0; i < arr.length - 1 - x; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            System.out.println("第" + (x + 1) + "次比较后：" + arrayToString(arr));
+        }
+    }
+
+    //把数组中的元素按照指定的规则组成一个字符串，[元素1, 元素2, ...]
+    public static String arrayToString(int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                sb.append(arr[i]);
+            } else {
+                sb.append(arr[i] + ", ");
+            }
+        }
+        sb.append("]");
+        String s = sb.toString();
+        return s;
+    }
+}
