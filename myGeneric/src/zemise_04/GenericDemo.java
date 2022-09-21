e07d54255c53afc0239a36e086c4b4b929218190
Wang Zhao <373933306@qq.com>
2022-09-21 17:27:33 +0800

    打卡 P253

---
+++ myGeneric/src/zemise_04/GenericDemo.java
@@ -0,0 +1,14 @@
+package zemise_04;
+/*
+    测试类
+ */
+public class GenericDemo {
+    public static void main(String[] args) {
+        Generic<String> g1 = new GenericImpl<>();
+        g1.show("林青霞");
+
+
+        Generic<Integer> g2 = new GenericImpl<>();
+        g2.show(99);
+    }
+}
---
+++ myGeneric/src/zemise_04/GenericImpl.java
@@ -0,0 +1,8 @@
+package zemise_04;
+
+public class GenericImpl<T> implements Generic<T> {
+    @Override
+    public void show(T t) {
+        System.out.println(t);
+    }
+}
