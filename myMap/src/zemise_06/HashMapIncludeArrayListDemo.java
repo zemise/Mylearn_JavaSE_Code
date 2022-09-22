23a2ae1088ba02317e413dc223e2db95e816d853
Wang Zhao <373933306@qq.com>
2022-09-22 17:31:28 +0800

    打卡 P265

---
+++ myMap/src/zemise_06/HashMapIncludeArrayListDemo.java
@@ -0,0 +1,34 @@
+package zemise_06;
+/*
+    需求，创建一个HashMap集合，存储三个键值对元素，每一个键值对元素的键是string，值是ArrayList， 每一个ArrayList的元素是String，并遍历
+
+思路：
+
+1：创建HashMap集合
+
+2：创建ArrayL ist集合，并添加元素
+
+3：把ArrayList作为元素添加到HashMap集合
+
+4：遍历HashMap集合给出如下的数据：
+
+第一个Arroylist集合的元素，（三国演义
+
+诸葛亮
+
+赵云
+
+第二个ArrayList集合的元素：（西游记）
+
+唐僧
+
+孙悟空
+
+第三个Arraylist集合的元素：（水浒传）
+
+武松
+
+鲁智深
+ */
+public class HashMapIncludeArrayListDemo {
+}
