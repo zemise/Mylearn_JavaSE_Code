d8f51a258f7b44ebea6053938200e8be2293bd04
Wang Zhao <373933306@qq.com>
2022-09-26 00:00:31 +0800

    打卡 288

--- myByteStream/src/zemise_03/BufferStreamDemo.java
+++ myByteStream/src/zemise_03/BufferStreamDemo.java
@@ -29,13 +29,19 @@ public class BufferStreamDemo {
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream("myByteStream/bos.txt"));
 
         //一次读取一个字节数据
-        int by;
-        while((by =bis.read())!= -1){
-            System.out.print((char)by);
+/*        int by;
+        while ((by = bis.read()) != -1) {
+            System.out.print((char) by);
 
-        }
+        }*/
 
         //一次读取一个字节数组数据
+        byte[] bys = new byte[1024];
+        int len;
+        while ((len = bis.read(bys)) != -1) {
+            System.out.print(new String(bys,0,len));
+        }
+
 
         //释放资源
         bis.close();
