37038cdc26bb7f405ef4dca5ca0cca0b5ed90748
Wang Zhao <373933306@qq.com>
2022-09-26 18:07:59 +0800

    打卡 P297

---
+++ myCharStream/src/zemise_03/CopyJavaDemo02.java
@@ -0,0 +1,21 @@
+package zemise_03;
+/*
+    需求：
+        把模块目录下的ConversionStreamDemo。java 复制到模块目录下的 Copy。 java
+
+数据源和目的地的分析
+数据源：myCharstreaml IConversionStreamDemo。java ---读数据 --- Reader --- InputStreamReader FileReader
+目的地：myCharstreamI1 Copy。 java 写数据 - Writer OutputStreamlriter Filelkriter
+
+思路：
+
+1：根据数据源创建字符输入流对家
+
+2：根据目的地创建字符输出流对象
+
+3：读写与数据，复制文件
+
+4：释放资源
+ */
+public class CopyJavaDemo02 {
+}
