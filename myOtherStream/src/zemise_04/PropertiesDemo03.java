6db99717b376ed14aaa6ff4689e96fa039d86821
Wang Zhao <373933306@qq.com>
2022-09-28 14:25:11 +0800

    打卡 322

--- myOtherStream/src/zemise_04/PropertiesDemo03.java
+++ myOtherStream/src/zemise_04/PropertiesDemo03.java
@@ -1,5 +1,7 @@
 package zemise_04;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 
 /*
@@ -7,11 +9,11 @@ import java.util.Properties;
         void Load (Reader reader);
             从输入字符流读取属性列表（键和元素对）
 
-        void store (writer writer, String comments):
+        void store (Writer writer, String comments):
             将此属性列表（键和元素对）写入此 Properties表中,以适合使用 Load(Reader)方法的格式写入输出字符流
  */
 public class PropertiesDemo03 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //把集合中的数据保存到文件中
         myStore();
         //把文件中的数据加载到集合
@@ -19,11 +21,17 @@ public class PropertiesDemo03 {
 
     }
 
-    private static void myStore() {
+    private static void myStore() throws IOException {
         Properties prop = new Properties();
         prop.setProperty("zemise001","林青霞");
         prop.setProperty("zemise002","张曼玉");
         prop.setProperty("zemise003","王祖贤");
+
+        //void store (Writer writer, String comments):
+        FileWriter fw = new FileWriter("myOtherStream/fw.txt");
+        prop.store(fw,null);
+        fw.close();
+
 
 
     }
