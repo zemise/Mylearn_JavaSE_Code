49e5b25a1e938beba49a78f7d61c7368b404d795
Wang Zhao <373933306@qq.com>
2022-10-13 23:52:38 +0800

    sql

--- mySQL/src/zemise_01/ConnDemo.java
+++ mySQL/src/zemise_01/ConnDemo.java
@@ -3,6 +3,7 @@ package zemise_01;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnDemo {
     //声明Connection对象
@@ -11,7 +12,7 @@ public class ConnDemo {
     public Connection getConnection(){
         //加载数据库驱动类
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("数据库驱动加载成功");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -21,13 +22,13 @@ public class ConnDemo {
         try {
             //通过访问数据库的URL，获取数据库连接对象
             con = DriverManager.getConnection("jdbc:mysql:" +
-                    "//cellcraft.123",
-                    "root","123");
+                    "//cellcraft.####:3306/cellcraft",
+                    "root","#####");
             System.out.println("数据库连接成功");
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //按要求返回一个Connecttion对象
+        //按要求返回一个Connection对象
         return con;
     }
 
@@ -35,6 +36,23 @@ public class ConnDemo {
         //创建本类对象
         ConnDemo c = new ConnDemo();
         //调用连接数据库方法
-        c.getConnection();
+        Connection connection = c.getConnection();
+        try {
+            //要执行SQL语句，首先要获得Statement类对象
+            Statement sql = connection.createStatement();
+            //查询的结果集存放在ResultSet类对象中
+            ResultSet resultSet = sql.executeQuery("select *from authme");
+            while(resultSet.next()){
+                String username = resultSet.getString("username");
+                String ip = resultSet.getString("ip");
+                System.out.println("name: " + username);
+                System.out.println("ip: " + ip);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
